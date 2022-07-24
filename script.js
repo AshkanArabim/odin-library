51af962e54488ae16120314c56207ed7ca8a05f4
AshkanArabim <ashkan.arabim@gmail.com>
2022-07-24 12:57:29 -0600

    remove isFinished input

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let library = [];
 
-function makeBook(title, author, pages, pagesRead, isFinished) {
+function makeBook(title, author, pages, pagesRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.pagesRead = pagesRead;
-    this.isFinished = isFinished;
+    if (this.pages === this.pagesRead) {
+        this.isFinished = true;
+    } else {
+        this.isFinished = false;
+    }
 }
 
 function bookToLibrary() {
