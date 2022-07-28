f799598d4c61b3062be9953b27d78eaa10cd5e10
AshkanArabim <ashkan.arabim@gmail.com>
2022-07-28 09:49:18 -0600

    fix ascending/descending order

--- script.js
+++ script.js
@@ -64,6 +64,16 @@ function renderBooks(){
     }
 }
 
+function listsEqual(list1, list2) {
+    for (let index in list1) {
+        if (list1[index] !== list2[index]) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+
 form.addEventListener('submit', () => {
     subBtn.textContent = 'Add Book';
     const formTitle = document.querySelector('#title').value;
@@ -83,17 +93,32 @@ form.addEventListener('submit', () => {
 
 for (let i=0; i<5; i++) {
     window[btnNames[i]].addEventListener('click', () => {
+
+        const tempLib = [...library];
+
         library.sort((a,b) => {
             if(a[attrs[i]] < b[attrs[i]]) {return -1;}
             else if (a[attrs[i]] > b[attrs[i]]) {return 1;}
             else {return 0;};
         })
+        
         for (let i=0; i<5; i++) {
             let btnText = window[btnNames[i]];
             btnText.textContent = btnText.textContent.replaceAll('⇑', '');
             btnText.textContent = btnText.textContent.replaceAll('⇓', '');
         }
         window[btnNames[i]].textContent = '⇑' + window[btnNames[i]].textContent;
+
+        if (listsEqual(library, tempLib)) {
+            console.log('they match!')
+            library = library.reverse();
+            if (window[btnNames[i]].textContent.search('⇑')) {
+                window[btnNames[i]].textContent = window[btnNames[i]].textContent.replaceAll('⇑','⇓');
+            } else if (window[btnNames[i]].textContent.search('⇓')) {
+                window[btnNames[i]].textContent = window[btnNames[i]].textContent.replaceAll('⇓','⇑');
+            }
+        }
+
         renderBooks()
     });
 }
